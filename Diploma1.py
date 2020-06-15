15a7244b57aabf092a7c0063043bd850e27434f2
NATKV <58748026+NATKV@users.noreply.github.com>
2020-06-15 14:39:09 +0300

    Update Diploma1.py

--- Diploma1.py
+++ Diploma1.py
@@ -6,7 +6,6 @@ token = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c
 
 def get_params(add_params: dict = None):
     params = {"access_token": token, "v": 5.107}
-
     if add_params:
         for key in add_params:
             params[key] = add_params[key]
@@ -17,29 +16,27 @@ def get_request(method: str, params):
     url = f'https://api.vk.com/method/{method}'
     response = requests.get(url, params)
     result = response.json()
-    # туту можно сделать предварительную проверку на ошибки и вернуть json дальше уже каждая функция сама пусть разбирает ответ в зависимости от логики
     return result
 
 
 def get_user_id(short_name):
-    params = {}
-    params["user_ids"] = short_name
-    params["extended"] = "1"
-    parameters = get_params(params)
+    special_params = {}
+    special_params["user_ids"] = short_name
+    special_params["extended"] = "1"
+    parameters = get_params(special_params)
     new_method = "users.get"
-    url_method = get_request(new_method, parameters)
-    response = requests.get(url_method, parameters)
-    return response.json()["response"][0]["id"]
+    response = get_request(new_method, parameters)
+    print(response)
+    return response["response"][0]["id"]
 
 
 def list_of_friends(id):
-    params = {}
-    params["user_id"] = id
-    parameters = get_params(params)
+    special_params = {}
+    special_params["user_id"] = id
+    parameters = get_params(special_params)
     new_method = "friends.get"
-    url_method = get_request(new_method, parameters)
-    response = requests.get(url_method, parameters)
-    json = response.json()["response"]
+    response = get_request(new_method, parameters)
+    json = response["response"]
     if "items" in json:
         friends_list = []
         for each_friend in json["items"]:
@@ -51,14 +48,13 @@ def list_of_friends(id):
 
 
 def get_groups(id):
-    params = {}
-    params["user_id"] = id
-    parameters = get_params(params)
+    special_params = {}
+    special_params["user_id"] = id
+    parameters = get_params(special_params)
     new_method = "groups.get"
-    url_method = get_request(new_method, parameters)
-    response = requests.get(url_method, parameters)
-    if "response" in response.json():
-        json = response.json()["response"]
+    response = get_request(new_method, parameters)
+    if "response" in response:
+        json = response["response"]
         if "items" in json:
             groups_list = json["items"]
             return groups_list
@@ -66,21 +62,20 @@ def get_groups(id):
             print("Error:", json)
 
     else:
-        print("Error:", response.json())
+        print("Error:", response)
     return []
 
 
 def info_groups(id):
     try:
-        params = {}
-        params["group_id"] = id
-        params["fields"] = "members_count"
-        parameters = get_params(params)
+        special_params = {}
+        special_params["group_id"] = id
+        special_params["fields"] = "members_count"
+        parameters = get_params(special_params)
         new_method = "groups.getById"
-        URL_method = get_request(new_method, parameters)
-        response = requests.get(URL_method, parameters)
-        return {"name": response.json()["response"][0]["name"], "gid": id,
-                "members_count": response.json()["response"][0]["members_count"]}
+        response = get_request(new_method, parameters)
+        return {"name": response["response"][0]["name"], "gid": id,
+                "members_count": response["response"][0]["members_count"]}
 
     except KeyError:
         return {}
